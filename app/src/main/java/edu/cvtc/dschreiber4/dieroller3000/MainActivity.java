0573b04a510bb726fc4faceb65fd4a43b8e20b95
dschreiber4 <dschreiber4@student.cvtc.edu>
2021-10-05 14:33:16 -0500

    push DicePage layout

--- app/src/main/java/edu/cvtc/dschreiber4/dieroller3000/MainActivity.java
+++ app/src/main/java/edu/cvtc/dschreiber4/dieroller3000/MainActivity.java
@@ -2,7 +2,9 @@ package edu.cvtc.dschreiber4.dieroller3000;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void DicePage(View view) {
+        //Create intent and navigate to new activity
+        Intent intent = new Intent(getApplicationContext(), DicePage.class);
+        startActivity(intent);
+    }
+
+    public void Characters(View view) {
+        //Create intent and navigate to new activity
+        Intent intent = new Intent(getApplicationContext(), Characters.class);
+        startActivity(intent);
     }
 }
