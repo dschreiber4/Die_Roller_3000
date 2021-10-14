b25c730879924c658c909670fb40f248d8e86f62
dschreiber4 <dschreiber4@student.cvtc.edu>
2021-10-13 20:44:58 -0500

    RB and roll button working

--- app/src/main/java/edu/cvtc/dschreiber4/dieroller3000/DicePage.java
+++ app/src/main/java/edu/cvtc/dschreiber4/dieroller3000/DicePage.java
@@ -1,14 +1,73 @@
 package edu.cvtc.dschreiber4.dieroller3000;
 
+import static java.lang.Math.random;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
 public class DicePage extends AppCompatActivity {
+
+
+    private int dieResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dice_page);
+
+
+
+        final Button rollBtn = findViewById(R.id.roll_button);
+        rollBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                RadioGroup RG = findViewById(R.id.dice_RGroup);
+                TextView result = findViewById(R.id.roll_result_textView);
+
+
+                switch (RG.getCheckedRadioButtonId()) {
+                    case R.id.rb_4_sided:
+                        dieResult = (int)(4* random())+1;
+                        result.setText(String.valueOf(dieResult));
+                        break;
+                    case R.id.rb_6_sided:
+                        dieResult = (int)(6* random())+1;
+                        result.setText(String.valueOf(dieResult));
+                        break;
+                    case R.id.rb_8_sided:
+                        dieResult = (int)(8* random())+1;
+                        result.setText(String.valueOf(dieResult));
+                        break;
+                    case R.id.rb_10_sided:
+                        dieResult = (int)(10* random())+1;
+                        result.setText(String.valueOf(dieResult));
+                        break;
+                    case R.id.rb_12_sided:
+                        dieResult = (int)(12* random())+1;
+                        result.setText(String.valueOf(dieResult));
+                        break;
+                    case R.id.rb_20_sided:
+                        dieResult = (int)(20* random())+1;
+                        result.setText(String.valueOf(dieResult));
+                        break;
+                    case R.id.rb_100_sided:
+                        dieResult = (int)(100* random())+1;
+                        result.setText(String.valueOf(dieResult));
+                        break;
+                    default:
+                        dieResult = 0;
+                        result.setText("Choose a Die");
+                }
+
+            }
+        });
+
     }
+
 }
